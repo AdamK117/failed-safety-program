23abde6f63bf2f9411ca5952bfa8ff53daa35cd3
Adam <adamk117@gmail.com>
2013-11-09 16:04:03 +0000

    a

---
+++ SafetyProgram.Base/Helpers.fs
@@ -0,0 +1,9 @@
+﻿module SafetyProgram.Base.Helpers
+
+open System
+
+let NullCheck([<ParamArray>] arr : 'a array) = 
+    arr
+    |> Array.iter(fun a ->
+        if a = null then
+            raise(new ArgumentNullException()))
---
+++ SafetyProgram.Core.Commands/CommandCore.fs
@@ -0,0 +1,9 @@
+﻿module SafetyProgram.Core.CommandCore
+
+open SafetyProgram.Core
+
+type command<'a> = {
+    CanExecute : 'a -> bool
+    Execute : 'a -> Option<'a->'a>
+}
+
